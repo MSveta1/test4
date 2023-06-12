37e25d1c9ce20539e73da5fa1cbf2499650af75a
MSveta1 <crav4enko.vet@yandex.ru>
2023-06-12 03:22:04 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int miles = cost / 20;
+
+        if (cost < 0) {
+            cost = 0;
+        }
+        return miles;
+    }
+}
+
